7ed0355cbcb581ca6194ab1b2eda0a4ae826610d
Sonsyduke <douglaslombard29@gmail.com>
2022-04-01 14:33:44 +0200

    Finished the styling

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// const form = document.querySelector("#form");
+// const email = document.querySelector("#email");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   checkInputs();
+// })
+
+// function checkInputs() {
+//   // get the values from the inputs
+//   const emailValue = email.value.trim();
+
+//   if(emailValue === "") {
+//     // show error
+//     // add error class
+//   } else {
+//     // add success class
+//   }
+// }
